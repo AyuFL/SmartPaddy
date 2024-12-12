05862948a210dd655a1cb894edca66d7ed52507f
bella saharani <intern.bella@gli.id>
2024-12-12 10:27:20 +0700

    add firebase app distribution

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
   alias(libs.plugins.android.application)
   alias(libs.plugins.kotlin.android)
+  id("com.google.gms.google-services")
 }
 
 android {
@@ -71,4 +72,8 @@ dependencies {
 
   // datastore
   implementation(libs.androidx.datastore.preferences)
+
+  // app distribution
+  implementation(platform(libs.firebase.bom))
+  implementation(libs.firebase.analytics)
 }
